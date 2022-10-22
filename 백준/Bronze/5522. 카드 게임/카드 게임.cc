01e345139cdee923dd3c02834bd1fe3221c83b1e
dostiny <67443718+dostiny@users.noreply.github.com>
2022-10-22 23:12:02 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int sum = 0;
+  int input;
+  for(int i = 0; i < 5; i++) {
+    cin >> input;
+    sum += input;
+  }
+  cout << sum;
+  return 0;
+}
