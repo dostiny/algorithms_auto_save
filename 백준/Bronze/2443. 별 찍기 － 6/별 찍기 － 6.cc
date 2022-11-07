5550ca8b5745c48fb373bc3167c8b175ec6d7f24
dostiny <67443718+dostiny@users.noreply.github.com>
2022-11-08 01:37:39 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, last;
+    cin >> n;
+    last = n * 2 - 1;
+    char arr[last];
+    for (int k = n-1; k >= 0; --k) {
+        for (int i = 0; i < n + k; ++i) {
+            if (n - 1 - k <= i && i <= n - 1 + k) {
+                arr[i] = '*';
+            } else {
+                arr[i] = ' ';
+            }
+            cout << arr[i];
+        }
+        cout << "\n";
+    }
+    return 0;
+}
