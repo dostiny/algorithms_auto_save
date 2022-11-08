e81745576567291a517b37d8ec34e7a93f815635
dostiny <67443718+dostiny@users.noreply.github.com>
2022-11-09 01:04:47 +0900

    [Silver V] Title: 소수, Time: 156 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2581. 소수/소수.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int M, N, sum = 0, min = -1, cnt = 0;
+    cin >> M >> N;
+
+    for (int i = M; i <= N; i++) {
+        for (int div = 1; div <= i; div++) {
+            if (i % div == 0)
+                cnt++;
+        }
+        if (cnt == 2) {
+            if (min == -1)
+                min = i;
+            sum += i;
+        }
+        cnt = 0;
+    }
+    if (min == -1)
+        cout << -1 << '\n';
+    else
+        cout << sum << '\n' << min << '\n';
+}
