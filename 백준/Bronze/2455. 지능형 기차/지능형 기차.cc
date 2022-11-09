140701f40d7121eeb150b8f986b81b9d34def519
dostiny <67443718+dostiny@users.noreply.github.com>
2022-11-10 00:49:54 +0900

    [Bronze III] Title: 지능형 기차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2455. 지능형 기차/지능형 기차.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    int sum = 0;
+    int max = 0;
+
+    for (int i = 0; i < 4; i++) {
+        scanf("%d %d", &a, &b);
+        sum += -a + b;
+        if (sum > max)
+            max = sum;
+    }
+    printf("%d", max);
+}
