b66764ed1ddbe821a30a9e16d0fe466c6542763d
dostiny <67443718+dostiny@users.noreply.github.com>
2022-11-30 01:03:39 +0900

    [Silver IV] Title: 큐, Time: 100 ms, Memory: 32460 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,32 @@
+import sys;
+from collections import deque
+
+N = int(sys.stdin.readline())
+queue = deque()
+
+for _ in range(N):
+    arr = list(sys.stdin.readline().split())
+    if arr[0] == 'push':
+        queue.append(int(arr[1]))
+    elif arr[0] == 'pop':
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
+    elif arr[0] == 'size':
+        print(len(queue))
+    elif arr[0] == 'empty':
+        if queue:
+            print(0)
+        else:
+            print(1)
+    elif arr[0] == 'front':
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+    elif arr[0] == 'back':
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)
