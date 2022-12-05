b7ad673445841184963d218627aa3062dc6c21d5
dostiny <67443718+dostiny@users.noreply.github.com>
2022-12-06 02:14:28 +0900

    [Bronze V] Title: 크냐?, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/4101. 크냐？/크냐？.py
+++ 백준/Bronze/4101. 크냐？/크냐？.py
@@ -1,12 +1,8 @@
-numli = []
 while True:
     N, M = map(int, input().split())
     if N == 0 and M == 0:
         break
+    elif N > M:
+        print('Yes')
     else:
-        numli.append((N, M))
-for N, M in numli:
-    if N > M:
-        print("Yes")
-    else:
-        print("No")
+        print('No')
