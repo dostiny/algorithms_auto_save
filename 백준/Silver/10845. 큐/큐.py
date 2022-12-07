32fbe089fe5008147bf58e4e43b4bfc5d404d7ae
dostiny <67443718+dostiny@users.noreply.github.com>
2022-12-07 16:34:19 +0900

    [Silver IV] Title: 큐, Time: 80 ms, Memory: 34092 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -2,31 +2,31 @@ import sys;
 from collections import deque
 
 N = int(sys.stdin.readline())
-queue = deque()
+Q = deque()
 
 for _ in range(N):
     arr = list(sys.stdin.readline().split())
     if arr[0] == 'push':
-        queue.append(int(arr[1]))
+        Q.append(int(arr[1]))
     elif arr[0] == 'pop':
-        if queue:
-            print(queue.popleft())
+        if Q:
+            print(Q.popleft())
         else:
             print(-1)
     elif arr[0] == 'size':
-        print(len(queue))
+        print(len(Q))
     elif arr[0] == 'empty':
-        if queue:
+        if Q:
             print(0)
         else:
             print(1)
     elif arr[0] == 'front':
-        if queue:
-            print(queue[0])
+        if Q:
+            print(Q[0])
         else:
             print(-1)
     elif arr[0] == 'back':
-        if queue:
-            print(queue[-1])
+        if Q:
+            print(Q[-1])
         else:
             print(-1)
