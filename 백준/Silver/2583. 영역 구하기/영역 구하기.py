27567df299bb5f95fdf93750099d18be57fd34b5
dostiny <67443718+dostiny@users.noreply.github.com>
2022-12-19 09:44:21 +0900

    [Silver I] Title: 영역 구하기, Time: 104 ms, Memory: 32436 KB -BaekjoonHub

---
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+def bfs():
+    global cnt
+    while Q:
+        r, c = Q.popleft()
+        for i in range(4):
+            nr, nc = r + dy[i], c + dx[i]
+            if 0 <= nr < M and 0 <= nc < N and arr[nr][nc] == 0:
+                Q.append((nr, nc))
+                arr[nr][nc] = -1
+                cnt += 1
+
+M, N, K = map(int, input().split())
+arr = [[0] * N for _ in range(M)]
+for _ in range(K):
+    x1, y1, x2, y2 = map(int, input().split())
+    for j in range(y1, y2):
+        for i in range(x1, x2):
+            arr[j][i] = 1
+dy = [-1, 1, 0, 0]
+dx = [0, 0, 1, -1]
+Q = deque()
+cnt, ans = 0, []
+for r in range(M):
+    for c in range(N):
+        if arr[r][c] == 0:
+            arr[r][c] = -1
+            cnt = 1
+            Q.append((r, c))
+            bfs()
+            ans.append(cnt)
+print(len(ans))
+print(*sorted(ans))
