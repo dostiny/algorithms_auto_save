505d8d0ea1bf50058b3cf580c82db9b24ca9015f
dostiny <67443718+dostiny@users.noreply.github.com>
2023-07-31 15:10:47 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -1,17 +1,4 @@
 def solution(my_string, overwrite_string, s):
-    answer = ''
     n = len(overwrite_string)
-    m, j = 0, 0
-    for i in range(len(my_string)):
-        if i == s:
-            m = 1
-
-        if m == 0:
-            answer += my_string[i]
-        elif m == 1:
-            answer += overwrite_string[j]
-            j += 1
-            if j == n:
-                m = 0
+    answer = my_string[:s] + overwrite_string + my_string[s + n:]
     return answer
-
