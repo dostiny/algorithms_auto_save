f89f9c3dd37c8000c238a780fed811a8102ab760
dostiny <67443718+dostiny@users.noreply.github.com>
2023-09-18 16:24:21 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.py
@@ -0,0 +1,12 @@
+def solution(id_pw, db):
+    answer = ''
+    for i in db:
+        if id_pw[0] == i[0]:
+            if id_pw[1] == i[1]:
+                answer = 'login'
+                break
+            else:
+                answer = 'wrong pw'
+    if answer == '':
+        answer = 'fail'
+    return answer
