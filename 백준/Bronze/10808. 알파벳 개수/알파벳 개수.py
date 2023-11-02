149eb83ac0a8bf82f5b2a5235841411cdea95010
dostiny <67443718+dostiny@users.noreply.github.com>
2023-11-02 11:31:12 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -4,12 +4,16 @@ input = sys.stdin.readline
 
 S = input().strip()
 
-alpha = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25}
+alpha = 'abcdefghijklmnopqrstuvwxyz'
+aldic = {}
+
+for i in range(len(alpha)):
+    aldic[alpha[i]] = i
 
 arr = [0] * 26
 
 for i in S:
-    arr[alpha[i]] += 1
+    arr[aldic[i]] += 1
 
 for j in arr:
     print(j, end=' ')
