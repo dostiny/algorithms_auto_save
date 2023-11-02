d8f055953100d8907ea9dc15a3b6aad16e4af1f1
dostiny <67443718+dostiny@users.noreply.github.com>
2023-11-03 00:03:39 +0900

    [Silver IV] Title: 큐, Time: 72 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -1,32 +1,36 @@
-import sys;
+import sys
 from collections import deque
 
-N = int(sys.stdin.readline())
-Q = deque()
+input = sys.stdin.readline
+
+N = int(input())
+
+deq = deque()
 
 for _ in range(N):
-    arr = list(sys.stdin.readline().split())
-    if arr[0] == 'push':
-        Q.append(int(arr[1]))
-    elif arr[0] == 'pop':
-        if Q:
-            print(Q.popleft())
+    order = list(input().split())
+    if order[0] == 'push':
+        deq.append(order[1])
+    elif order[0] == 'pop':
+        if len(deq):
+            n = deq.popleft()
+            print(n)
         else:
             print(-1)
-    elif arr[0] == 'size':
-        print(len(Q))
-    elif arr[0] == 'empty':
-        if Q:
+    elif order[0] == 'size':
+        print(len(deq))
+    elif order[0] == 'empty':
+        if len(deq):
             print(0)
         else:
             print(1)
-    elif arr[0] == 'front':
-        if Q:
-            print(Q[0])
+    elif order[0] == 'front':
+        if len(deq):
+            print(deq[0])
         else:
             print(-1)
-    elif arr[0] == 'back':
-        if Q:
-            print(Q[-1])
+    elif order[0] == 'back':
+        if len(deq):
+            print(deq[-1])
         else:
             print(-1)
